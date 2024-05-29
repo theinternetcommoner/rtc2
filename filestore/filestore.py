0eb10b21806d95900e95618a3ff72202fde0930f
The Internet Commoner <theinternetcommoner@gmail.com>
2024-05-29 14:44:20 +0800

    File Upload Function
    add file upload function (uploads PDF files and save file name entry to database)

--- filestore/filestore.py
+++ filestore/filestore.py
@@ -19,7 +19,22 @@ def show_file(pdf_id):
 	file = tbl_file.query.get(pdf_id)
 	return render_template('pdf_file.html', file = file)
 
-# @file_store.route('file_upload', method = ['POST'])
-# def file_upload():
-# 	return 'uploads'
+@file_store.route('/file_upload', methods = ['POST'])
+def file_upload():
+	if 'pdf' not in request.files:
+		return redirect(request.url)
+
+	pdf = request.files['pdf']
+	if pdf.filename == '':
+		return redirect(request.url)
+
+	pdf.save(os.path.join('static/uploads', pdf.filename))
+	new_pdf = tbl_file(filename=pdf.filename)
+	db.session.add(new_pdf)
+	db.session.commit()
+	return redirect(url_for('filestore.dir'))
+
+@file_store.route('/uploadform')
+def upload_form():
+	return render_template('upload_form.html')
 # end of filestore functions
