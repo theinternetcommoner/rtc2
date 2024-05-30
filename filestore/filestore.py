7bb8eb144253c676f95c5997d5317ecade8d207b
The Internet Commoner <theinternetcommoner@gmail.com>
2024-05-30 08:31:13 +0800

    PDF Link and Gitignore
    added *.pdf to gitignore
    fix link and search function for pdf directory

--- filestore/filestore.py
+++ filestore/filestore.py
@@ -6,7 +6,7 @@ import os
 
 file_store = Blueprint("filestore", __name__, static_folder = "static", template_folder = "templates")
 
-# for filestore functions
+# read function (database and directory must have same value)
 @file_store.route('/')
 def dir():
 	filestorage = tbl_file.query.all()
@@ -14,11 +14,13 @@ def dir():
 	# 	return item.filename
 	return render_template('pdf_dir.html', storage = filestorage)
 
+#select function
 @file_store.route('/pdf/<int:pdf_id>')
 def show_file(pdf_id):
 	file = tbl_file.query.get(pdf_id)
 	return render_template('pdf_file.html', file = file)
 
+# file upload function
 @file_store.route('/file_upload', methods = ['POST'])
 def file_upload():
 	if 'pdf' not in request.files:
@@ -34,7 +36,25 @@ def file_upload():
 	db.session.commit()
 	return redirect(url_for('filestore.dir'))
 
+# upload form
 @file_store.route('/uploadform')
 def upload_form():
 	return render_template('upload_form.html')
-# end of filestore functions
+
+# file search function
+@file_store.route('/searchfile', methods = ['POST'])
+def searchfile():
+	searchval = '%'+request.form['search']+'%'
+	data = {
+		'filename': searchval
+	}
+
+	sql = """
+		SELECT * FROM tbl_file WHERE filename LIKE :filename
+	"""
+
+	query = text(sql)
+	result = db.session.execute(query, data)
+	rows = result.fetchall()
+
+	return render_template('pdf_dir.html', storage = rows)
